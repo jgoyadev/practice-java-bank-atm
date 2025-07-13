a00fd4aa99140c2a95d82310be759f7916497f5b
Josué Goya <jgoyadev@gmail.com>
2025-07-12 19:05:25 -0500

    Se agregó funcionalidad

--- src/main/java/com/local/java/bank/Main.java
+++ src/main/java/com/local/java/bank/Main.java
@@ -2,6 +2,6 @@ package com.local.java.bank;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to JavaBank ATM! Version control with Git.");
     }
 }
